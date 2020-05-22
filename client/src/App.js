3445e08ec151ae58395334acf2f2d511dbbd3120
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-22 08:49:13 -0700

    have the login page done and the user is now able to login with their token.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+      <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/Bubbles">Bubbles</Link>
+          </li>
+          {/* <li>
+            <Link to="/FriendForm">Form Page</Link>
+          </li> */}
+        </ul>
         <Route exact path="/" component={Login} />
+        <PrivateRoute exact path="/Bubbles" component={BubblePage} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+
       </div>
     </Router>
   );
